1aae735aa26458bac32445899c9f36cf4e9ac461
Daniela Mora <danielamora@Danielas-MacBook-Pro.local>
2025-07-01 19:09:43 -0500

    Nueva vista, partials de fotos y ruta con axios

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var usersRouter
+
 var indexRouter = require('./routes/index');
 
 var usersRouter = require('./routes/users');
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use('/', userRouter);
+app.use('/', usersRouter);
 
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+//importar axios para usar serivioc:
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,3 +9,22 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+// agrego la ruta paraa fotos en el mismo index
+router.get('/photos', async function(req, res, next) {
+  const URL = 'https://dawm-fiec-espol-default-rtdb.firebaseio.com/photos.json';
+
+  try {
+    //solo llamar asi!!!
+    
+    const response = await axios.get(URL);  // Llama a la API
+
+    res.render('fotos', { title: 'Fotos', fotos: response.data });
+
+  } catch (error) {
+    console.error('Error al obtener fotos:', error);
+    res.status(500).send('Error al cargar las fotos');
+  }
+});
+
+
